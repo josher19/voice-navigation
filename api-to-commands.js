f3a4bd87945de3380d911f52bc517f165f7a14ea
josher19 <josher19@gmail.com>
2020-06-21 23:38:03 -0700

    Fewer globals

--- api-to-commands.js
+++ api-to-commands.js
@@ -1,15 +1,21 @@
-const unCamelCase = word => word.replace(/([A-Z])/g, ' $1').trim();
-
-const getArgs = fcn => fcn.length ? ' *args' : '';
-
 // convert every function in an API to a voice command for annyang
-const apiToCommands(api) {
+function apiToCommands(api) {
+  const unCamelCase = word => word.replace(/([A-Z])/g, ' $1').trim();
+
+  const getArgs = fcn => fcn.length ? ' *args' : '';
+
+  const log = f => (...args) => logAndReturn(f(...args))
+
+  const logAndReturn = msg => { console.log(msg); return msg; }
+
   var cmds = {};
 
   for (const f in api) {
-    if (typeof api[f] === 'function') cmds[unCamelCase(f) + getArgs(api[f])] = api[f].bind(api);
+    if (typeof api[f] === 'function') cmds[unCamelCase(f) + getArgs(api[f])] = log(api[f].bind(api));
   }
 
+  if (!cmds['help']) cmds.help = () => console.log(Object.keys(cmds))
+  
   if (annyang) annyang.addCommands(cmds);
 
   return cmds;
